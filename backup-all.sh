8bbae6a43863578af752964c829fe3db0392b746
Fahad Yousuf <fahadysf@gmail.com>
2022-10-17 11:16:08 +0300

    Adding container data backup.

--- backup-all.sh
+++ backup-all.sh
@@ -4,36 +4,30 @@
 
 # Setting variables
 backup_path="/home/core/backups"
-#backup_path="/home/core/backups"
 tar_opts="--exclude='/var/run/*'"
 cd "${BASH_SOURCE%/*}" || exit
 
 mkdir -p $backup_path
 
 echo starting docker backup
+echo ""
 
+echo "- backing up container data (inspection output)"
+echo ""
+source backup-container-data.sh
 echo ""
 
 echo - backing up images
-
 echo ""
-
 source backup-images.sh
-
 echo ""
 
 echo - backing up volumes
-
 echo ""
-
 source backup-volumes.sh
-
 echo ""
 
 echo - upload to dropbox
-
 echo ""
-
 source sync-dropbox.sh
-
 echo ""
